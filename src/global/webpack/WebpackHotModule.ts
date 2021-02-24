ac537664ab73515a4c950cd76cfcdbd491cf1d37
waveslin <35038769+waveslin@users.noreply.github.com>
2021-02-23 23:30:50 -0500

    initial stage setup updated

---
+++ src/global/webpack/WebpackHotModule.ts
@@ -0,0 +1,25 @@
+/**
+ * Webpack HMR Activation
+ */
+
+type ModuleId = string | number;
+
+interface WebpackHotModule {
+  hot?: {
+    data: any;
+    accept(
+      dependencies: string[],
+      callback?: (updatedDependencies: ModuleId[]) => void,
+    ): void;
+    accept(dependency: string, callback?: () => void): void;
+    accept(errHandler?: (err: Error) => void): void;
+    dispose(callback: (data: any) => void): void;
+  };
+}
+
+// declare const module: WebpackHotModule;
+
+// if (module.hot) {
+//   module.hot.accept();
+//   module.hot.dispose(() => server.close());
+// }
--- src/index.ts
+++ src/index.ts
@@ -29,38 +29,8 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-
-/**
- * Webpack HMR Activation
- */
-
-type ModuleId = string | number;
-
-interface WebpackHotModule {
-  hot?: {
-    data: any;
-    accept(
-      dependencies: string[],
-      callback?: (updatedDependencies: ModuleId[]) => void,
-    ): void;
-    accept(dependency: string, callback?: () => void): void;
-    accept(errHandler?: (err: Error) => void): void;
-    dispose(callback: (data: any) => void): void;
-  };
-}
-
-declare const module: WebpackHotModule;
-
-if (module.hot) {
-  module.hot.accept();
-  module.hot.dispose(() => server.close());
-}
-
-
 /**
  * Server Activation
  */
 
-const server = app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
-  });
+app.listen(PORT, () => { console.log(`Listening on port ${PORT}`); });
